3c8d43e207a3b2026ff9a2e54b8166622d4ed9f9
Ashabrai Frauen <ashabraifrauen@yahoo.com>
2018-07-11 13:25:02 -0700

    Update on json and reset.css

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
 
-var name = prompt('Hello, what\'s your name?');
+var userName = prompt('Hello, what\'s your name?');
 
-console.log('name', + name);
-alert('Let\'s play a guessing game '+ name + '!');
+console.log('name', + userName);
+alert('Let\'s play a guessing game '+ userName + '!');
 
 
 
-var tattoos =Number(prompt( name +' how many tattoos do you think I have?'));
+var tattoos =Number(prompt( userName +' how many tattoos do you think I have?'));
 
 console.log('tattoos', tattoos);
 
 if(tattoos === 15) {
-    alert('Wow! You got it.');
-} 
-else if(tattoos > 15){
-    alert('Wrong, it\'s 15.');
+  alert('Wow! You got it.');
 }
-else 
+else if(tattoos > 15){
+  alert('Wrong, it\'s 15.');
+}
+else
 {
 
-    alert('Nice try, it\'s 15.');
-} 
+  alert('Nice try, it\'s 15.');
+}
 
 
 
@@ -28,39 +28,39 @@ var animal = prompt('So many people in Seattle own an animal, do you think I do 
 console.log('animal', animal);
 
 if (animal === 'N') {
-    alert('You are correct!');
+  alert('You are correct!');
 
 } else if (animal === 'Y')
 {
-    alert('You are wrong, but I sure do wish.');
+  alert('You are wrong, but I sure do wish.');
 }
 
 var outdoors = prompt('Living in the PNW most everyone likes being outdoors, can you guess if I do. Y or N?').toUpperCase();
 
 console.log('outdoors', outdoors);
 if(outdoors === 'N'){
-    alert ('That\'s right, I prefer the staying in a city.');
+  alert ('That\'s right, I prefer the staying in a city.');
 
 } else if (outdoors === 'Y')
 {
-    alert('Nope, I actually like being in a city more.');
+  alert('Nope, I actually like being in a city more.');
 }
 var favoriteColor =prompt('Now take a guess on what my favorite color is?').toUpperCase();
 console.log('favoriteColor', favoriteColor);
-    
+
 if(favoriteColor === 'GREEN'){
-        alert ('You got it!');
-    
+  alert ('You got it!');
+
 } else {
-    alert('Wrong! It\'s green.');
+  alert('Wrong! It\'s green.');
 }
 
 var condiment =prompt('Last question, can you guess what my favorite condiment is?').toUpperCase();
 console.log('condiment', + condiment);
 
 if(condiment ==='KETCHUP'){
-    alert('You are correct my friend!');
+  alert('You are correct my friend!');
 } else {
-    alert('You are wrong, it\'s ketchup!');
+  alert('You are wrong, it\'s ketchup!');
 }
 
